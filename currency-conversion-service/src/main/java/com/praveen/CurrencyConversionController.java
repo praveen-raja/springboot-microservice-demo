389ac5388932b3a2072ac9950465f654b756b495
praveen <praveen2099636@gmail.com>
2022-10-06 22:58:28 +0530

    Update currency conversion controller URL

--- currency-conversion-service/src/main/java/com/praveen/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/praveen/CurrencyConversionController.java
@@ -17,7 +17,7 @@ public class CurrencyConversionController {
 
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	@GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversion calculateCurrencyConversionByFeign(
 			@PathVariable String from, 
 			@PathVariable String to,
