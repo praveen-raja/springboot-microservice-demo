befb098049c3d89f6f3d98bdd45cbfb223e29829
praveen <praveen2099636@gmail.com>
2022-10-08 23:38:38 +0530

    Updated with docker compose and rabbitmq

--- currency-exchange-service/src/main/java/com/praveen/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/praveen/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.praveen;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
 	private Environment environment;
@@ -17,6 +21,8 @@ public class CurrencyExchangeController {
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+		
+		logger.info("Retrieving exchange value from {} -> {}", from, to);
 		
 		// obtaining server port details thru environment
 		// save it in environmentPort
